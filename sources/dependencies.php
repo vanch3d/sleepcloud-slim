5573cef1dda2a084b4ce8cb1d44f6aaf80552714
vanch3d <nicolas.github@calques3d.org>
2018-03-17 12:15:55 +0000

    removed global

--- sources/dependencies.php
+++ sources/dependencies.php
@@ -25,7 +25,7 @@ $container['view'] = function (\Slim\Container  $c) {
     $view->addExtension(new Twig_Extension_Debug());
     $view->addExtension(new Twig_Extension_Profiler($c['twig_profile']));
 
-    $view->getEnvironment()->addGlobal("current_path", $c["request"]->getUri()->getPath());
+    //$view->getEnvironment()->addGlobal("current_path", $c["request"]->getUri()->getPath());
     $view->getEnvironment()->addGlobal("app_name", getenv('APP_NAME'));
 
     return $view;
