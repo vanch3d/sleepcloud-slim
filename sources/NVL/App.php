9753855bc1a558275aa32fd9cf7b52fdf0e7233b
vanch3d <nicolas.github@calques3d.org>
2018-04-05 22:09:43 +0100

    added OpenAPI annotations

--- sources/NVL/App.php
+++ sources/NVL/App.php
@@ -8,6 +8,34 @@
 
 namespace NVL;
 
+use Swagger\Annotations as OAS;
+
+/**
+ *
+ * @OAS\OpenApi(
+ *     openapi= SWAGGER_VERSION,
+ *     @OAS\Info(
+ *          version= API_VERSION,
+ *          description="Description of the sleepcloud-slim APIs",
+ *          title=  API_NAME,
+ *          @OAS\License(
+ *              name="MIT",
+ *              url="https://github.com/vanch3d/sleepcloud-slim/blob/master/LICENSE"
+ *          ),
+ *          @OAS\Contact(
+ *              name="vanch3d",
+ *              url="https://github.com/vanch3d",
+ *              email="nicolas.github@calques3d.org"
+ *          ),
+ *     )
+ * )
+ *
+ * @OAS\Server(
+ *     description="sleepcloud-slim DEV server",
+ *     url="http://local.sleepcloud.org/api"
+ * )
+ *
+ */
 
 class App extends \Slim\App
 {
