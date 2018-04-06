e8425b8bea170fe35e78ad7338163d758843d922
vanch3d <nicolas.github@calques3d.org>
2018-04-06 13:40:22 +0100

    refactored dropbox access method added handler for openAPI json document added handler for iMood data added openAPI annotations to controller and methods

--- sources/NVL/Controllers/APIController.php
+++ sources/NVL/Controllers/APIController.php
@@ -15,12 +15,15 @@ use Kunnu\Dropbox\Dropbox;
 use Kunnu\Dropbox\DropboxApp;
 use Kunnu\Dropbox\Exceptions\DropboxClientException;
 use League\Csv\Reader;
-use NVL\SleepCloud\Event;
+use NVL\Models\SleepCloud\Event;
+use RarArchive;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Tracy\Debugger;
+use ZipArchive;
+use Swagger\Annotations as OAS;
 
 class JSONDateTime extends DateTime implements \JsonSerializable
 {
@@ -36,11 +39,35 @@ class JSONDateTime extends DateTime implements \JsonSerializable
     }
 }
 
+/**
+ * Class APIController
+ * @package NVL\Controllers
+ * @todo[vanch3d] Break the controller up, separating data sources in modules
+ *
+ * @OAS\Tag(
+ *     name="SleepCloud",
+ *     description="Sleep cycle tracker for Android",
+ *     @OAS\ExternalDocumentation(
+ *         description="Read more",
+ *         url="https://sleep.urbandroid.org/"
+ *     )
+ * )
+ *
+ * @OAS\Tag(
+ *     name="iMood",
+ *     description="Mood tracker for Android",
+ *     @OAS\ExternalDocumentation(
+ *         description="Read more",
+ *         url="https://www.imoodjournal.com/"
+ *     )
+ * )
+ *
+ */
 class APIController extends Controller
 {
     private $cachepath = DIR . '.cache/.sleep/';
 
-    private function getFromDropBox()
+    private function getFromDropBox(string $dropboxfilename,string $ext)
     {
         $dbapp = new DropboxApp(
             null,
@@ -52,27 +79,34 @@ class APIController extends Controller
         $filename = null;
         try {
             //Get File Metadata
-            $fileMetadata = $dropbox->getMetadata(getenv("SLEEPCLOUD_DATAFILE"));
+            $fileMetadata = $dropbox->getMetadata($dropboxfilename);
             Debugger::barDump($fileMetadata);
 
             $hash = $fileMetadata->getData()['content_hash'];
-            $filename = $this->cachepath . $hash . '.csv';
+            $filename = $this->cachepath . $hash . $ext;
+            $this->getLogger()->notice("check data hash value",array(
+                'hash' => $hash));
 
             // check if hashed cache exists
             if (!file_exists($filename)) {
+                $this->getLogger()->notice("file does not exist. clean");
+
                 // clear cache folder
                 $di = new RecursiveDirectoryIterator($this->cachepath, FilesystemIterator::SKIP_DOTS);
                 $ri = new RecursiveIteratorIterator($di, RecursiveIteratorIterator::CHILD_FIRST);
                 foreach ( $ri as $file ) {
-                    $file->isDir() ?  rmdir($file) : unlink($file);
+                   // $file->isDir() ?  rmdir($file) : unlink($file);
                 }
-                $dropbox->download(getenv("SLEEPCLOUD_DATAFILE"), $filename);
+                $dropbox->download($dropboxfilename, $filename);
+                $this->getLogger()->notice("file downloaded from dropbox");
             }
 
 
         } catch (DropboxClientException $e) {
             // @todo[vanch3d] check for exception handling
         }
+        $this->getLogger()->notice("file downloaded from dropbox",array(
+                    'file' => $filename));
 
         return $filename;
     }
@@ -138,10 +172,60 @@ class APIController extends Controller
      * @param Response $response
      * @return Response
      */
+    public function getOpenAPI(Request $request, Response $response)
+    {
+        $json = json_decode(@file_get_contents(DIR . 'openapi.json'),true);
+        if (!$json) {
+            // @todo[vanch3d] Add proper error message (see API Problem Details & Crell/ApiProblem)
+            return $response->withJson([], 404);
+        }
+        return $response->withJson($json, 200)
+            ->withHeader('Access-Control-Allow-Origin', '*');
+    }
+
+    /**
+     * @OAS\Get(
+     *     path="/records/sleep",
+     *     summary="Get the sleep data from the sleepcloud repository",
+     *     tags={"SleepCloud"},
+     *     description="",
+     *     operationId="getSleepData",
+     *     @OAS\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OAS\MediaType(
+     *              mediaType="application/json",
+     *              @OAS\Schema(
+     *                  @OAS\Property(
+     *                      property="data",
+     *                      type="array",
+     *                      @OAS\Items(
+     *                          ref="#/components/schemas/Sleep"
+     *                      ),
+     *                  ),
+     *                  @OAS\Property(
+     *                      property="metadata",
+     *                      type="object"
+     *                  ),
+     *                  @OAS\Property(property="errors",type="object"),
+     *              ),
+     *          ),
+     *     )
+     * )
+     *
+     *
+     * @param Request  $request
+     * @param Response $response
+     * @return Response
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
     public function getSleepData(Request $request, Response $response)
     {
-        $cache = $this->getFromDropBox();
+        $this->getLogger()->notice("start loading data");
+        $cache = $this->getFromDropBox(getenv("SLEEPCLOUD_DATAFILE"),".csv");
         $hash = basename($cache, ".csv");
+        $this->getLogger()->notice("load cached data");
 
         /** @var Reader $csv */
         $csv = Reader::createFromPath($cache, 'r');
@@ -203,4 +287,93 @@ class APIController extends Controller
         ], 200);
     }
 
+    /**
+     *
+     * @OAS\Get(
+     *     path="/records/mood",
+     *     summary="Get the mood data from the iMood Journal repository",
+     *     tags={"iMood"},
+     *     description="",
+     *     operationId="getMoodData",
+     *     @OAS\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OAS\MediaType(
+     *              mediaType="application/json",
+     *              @OAS\Schema(
+     *                  @OAS\Property(
+     *                      property="data",
+     *                      type="array",
+     *                      @OAS\Items(
+     *                          ref="#/components/schemas/Mood"
+     *                      ),
+     *                  ),
+     *                  @OAS\Property(
+     *                      property="metadata",
+     *                      type="object",
+     *                      @OAS\Property(
+     *                          property="hash",
+     *                          type="string"
+     *                      ),
+     *                      @OAS\Property(
+     *                          property="tags",
+     *                          type="array",
+     *                          @OAS\Items(
+     *                              ref="#/components/schemas/MoodTag"
+     *                          )
+     *                      )
+     *                  ),
+     *                  @OAS\Property(property="errors",type="object"),
+     *              ),
+     *          ),
+     *     )
+     * )
+     *
+     * @param Request  $request
+     * @param Response $response
+     * @return Response
+     */
+    public function getMoodData(Request $request, Response $response)
+    {
+        $cache = $this->getFromDropBox(getenv("IMOOD_DATAFILE"),".zip");
+        $hash = basename($cache, ".zip");
+
+        $zip = new ZipArchive;
+        $res = $zip->open($cache);
+        for( $i = 0; $i < $zip->numFiles; $i++ ){
+            $metadata['list'][] = $zip->statIndex( $i );
+        }
+
+        $metadata = [];
+        $data = [];
+        $tags = [];
+
+        $im_string = $zip->getFromName("/backup.json");
+        $data = json_decode($im_string,true);
+
+        foreach ($data as &$records)
+        {
+            $alltags = [];
+            foreach ($records['moodTags'] as $moodtag) {
+                $name = $moodtag['tagName'];
+                if (!$tags[$name])
+                {
+                    $tags[$name] = $moodtag;
+                }
+                $alltags[]= $name;
+            }
+            unset($records['moodTags']);
+            $records['moodTags'] = $alltags;
+
+        }
+
+        $metadata['hash'] = $hash;
+        $metadata['tags'] = $tags;
+
+        return $response->withJson([
+            'data' => $data,
+            'metadata' => $metadata
+        ], 200);
+
+    }
 }
