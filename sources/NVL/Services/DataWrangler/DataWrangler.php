c02f93afd5bdd1b461c6f13638f17bbacfef6f4e
vanch3d <nicolas.github@calques3d.org>
2018-05-14 22:42:56 +0100

    Added interface for data wrangles

---
+++ sources/NVL/Services/DataWrangler/DataWrangler.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vanch3d <nicolas.github@calques3d.org>
+ * Date: 14/05/2018
+ * Time: 20:14
+ */
+
+namespace NVL\Services\DataWrangler;
+
+
+interface DataWrangler
+{
+
+}
