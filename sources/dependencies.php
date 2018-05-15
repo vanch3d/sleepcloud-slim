04469aee0fb9ac20f34c3afc33daadc81dd155c7
vanch3d <nicolas.github@calques3d.org>
2018-05-15 07:13:43 +0100

    added sleep data service to container

--- sources/dependencies.php
+++ sources/dependencies.php
@@ -94,3 +94,14 @@ $container['mood'] = function (\Slim\Container  $c) {
     $dropbox = $c->get('dropbox');
     return new DataWrangler\Mood($settings['dropbox'],$c->logger,$dropbox);
 };
+
+/**
+ * @param \Slim\Container $c
+ * @return DataWrangler\Sleep
+ * @throws \Interop\Container\Exception\ContainerException
+ */
+$container['sleep'] = function (\Slim\Container  $c) {
+    $settings = $c->get('settings');
+    $dropbox = $c->get('dropbox');
+    return new DataWrangler\Sleep($settings['dropbox'],$c->logger,$dropbox);
+};
