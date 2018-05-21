12ea41c7827438d2ea73a316a78c0f42fe96e4af
vanch3d <nicolas.github@calques3d.org>
2018-05-21 17:39:18 +0100

    added extraction of food types added support for a nutrient data provider a bit of documentation and cleaning

--- sources/NVL/Services/DataWrangler/Diet.php
+++ sources/NVL/Services/DataWrangler/Diet.php
@@ -7,22 +7,45 @@
  */
 
 namespace NVL\Services\DataWrangler;
+use NVL\Services\DataProvider\ProviderInterface;
 
-
+/**
+ * Class Diet
+ * Extract diet-related information from the iMood Journal, using the Mood wrangler.
+ * @package NVL\Services\DataWrangler
+ */
 class Diet extends Mood
 {
     const BREAKFAST = "#breakfast";
     const LUNCH = "#lunch";
     const DINNER = "#dinner";
     const SNACKS = "#snacks";
+
+    // @todo[vanch3d] check if other keywords need to be identified
     const SANDWICH = "#sandwich";
 
-    protected $keywords = array(
+    /**
+     * @var array
+     */
+    protected $mealTypes = array(
         Diet::BREAKFAST,
         Diet::LUNCH,
         Diet::DINNER,
         Diet::SNACKS
     );
+
+    /**
+     * @var ProviderInterface $nutrients
+     */
+    protected $nutrients = null;
+
+    /**
+     * @param ProviderInterface $nutrients
+     */
+    public function setNutrientProvider(ProviderInterface $nutrients)
+    {
+        $this->nutrients = $nutrients;
+    }
 
     /**
      * @return array
@@ -37,7 +60,7 @@ class Diet extends Mood
             $arr = explode("\n", $comment);
 
             foreach ($arr as $item) {
-                $match = array_intersect(explode(' ', $item), $this->keywords);
+                $match = array_intersect(explode(' ', $item), $this->mealTypes);
                 if (count($match) > 0) {
                     $newRec = array(
                         'type' => $match[0],
@@ -61,8 +84,84 @@ class Diet extends Mood
     }
 
 
-    public function getFoodInfo()
+    public function getExtendedData($withNutrients=false)
     {
+        $json = $this->getData();
+        foreach ($json as $diet)
+            $data = array_merge($data ?? [], $diet['items']);
+        $data = array_merge([], array_unique($data));
 
+        // load food taxonomy
+        // @todo[vanch3d] to add to configuration
+        $fileCategories = DIR . "public/assets/food_category.json";
+        $tempCat = json_decode(@file_get_contents($fileCategories), true) ?? [];
+
+
+        $categories = [];
+        $errors = [];
+
+        // index food items
+        foreach ($tempCat as $cat)
+            $categories[$cat['id']] = $cat;
+
+        // assign group to items extracted from diary and identify uncategorised
+        foreach ($data as $foodItem) {
+            if (isset($categories[$foodItem]) &&
+                isset($categories[$foodItem]['group']) &&
+                $categories[$foodItem]['group'] !== null) continue;
+
+            $errors[] = $foodItem;
+            $categories[$foodItem] = array(
+                "id" => $foodItem,
+                "group" => null
+            );
+
+        }
+
+        // update the taxonomy with the new items
+        if (!empty($errors->category))
+            file_put_contents($fileCategories, json_encode($categories,JSON_PRETTY_PRINT));
+
+        // build output
+        //$json = array(
+        //    'data' => $categories,
+        //    'error' => $errors
+        //);
+
+        if ($withNutrients)
+        {
+            // if nutrients info service available, add them to data
+            $categories = $this->getNutrients($categories);
+        }
+
+        // extract all attributed food categories
+        $allCategories = array_values(array_unique(array_column($categories, "group")));
+
+        $json = array(
+            'data' => array_values($categories),
+            'metadata' => array(
+                'items' => $data,
+                'categories' => $allCategories
+            ),
+            'error' => $errors
+        );
+
+        return $json;
+
+    }
+
+    /**
+     * @param $wrap
+     * @return mixed
+     * @deprecated an alternative will be put in place through slim providers
+     * @todo[vanch3d] check for FatSecret or Edamam provider
+     */
+    private function getNutrients($wrap)
+    {
+        if ($this->nutrients !== null)
+        {
+            $this->nutrients->getHash("");
+        }
+        return $wrap;
     }
 }
