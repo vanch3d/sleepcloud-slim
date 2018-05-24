3c627ca48bdfe869d5170f17c2e4a4528a171ef4
vanch3d <nicolas.github@calques3d.org>
2018-05-24 09:36:08 +0100

    added migration/seed tools for sentinel

---
+++ database/SeedSentinel.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+/**
+ * Created by PhpStorm.
+ * User: vanch3d <nicolas.github@calques3d.org>
+ * Date: 24/05/2018
+ * Time: 09:34
+ */
+
+class SeedSentinel extends Seeder
+{
+    public function run()
+    {
+
+    }
+}
