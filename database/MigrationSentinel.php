ccbb11ff80b9ebe6980f158f069f98eae3bf51c2
vanch3d <nicolas.github@calques3d.org>
2018-05-24 09:33:28 +0100

    added migration tool for sentinel

---
+++ database/MigrationSentinel.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vanch3d <nicolas.github@calques3d.org>
+ * Date: 23/05/2018
+ * Time: 14:19
+ */
+
+use Illuminate\Database\Capsule\Manager;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class MigrationSentinel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+
+}
