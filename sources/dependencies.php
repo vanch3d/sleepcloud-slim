d92afd4f714db94dad1b2867d8ce12dd7837dbc4
vanch3d <nicolas.github@calques3d.org>
2018-05-25 13:58:09 +0100

    added Auth wrapper to Twig globals added services to the Slim container: - Sentinel (hasher, dispatcher) - Auth wrapper fixed container changes

--- sources/dependencies.php
+++ sources/dependencies.php
@@ -6,7 +6,10 @@
  * Time: 23:17
  */
 
+use NVL\Container;
 use NVL\{
+    Auth\Auth,
+    Models\User,
     Services\DataProvider,
     Services\DataWrangler,
     Services\Tools,
@@ -16,6 +19,7 @@ use Slim\Flash\Messages;
 use Slim\Views\Twig;
 use Slim\Views\TwigExtension;
 
+/** @var Container $container */
 $container = $app->getContainer();
 
 // -----------------------------------------------------------------------------
@@ -27,10 +31,12 @@ $capsule->setAsGlobal();
 $capsule->bootEloquent();
 
 // -----------------------------------------------------------------------------
-// Service providers
+// Slim main service providers
 // -----------------------------------------------------------------------------
+
 // Twig
-$container['view'] = function (\Slim\Container  $c) {
+$container['view'] = function (Container $c) {
+
     $settings = $c->get('settings');
     $view = new Twig($settings['view']['template_path'], $settings['view']['twig']);
     // Add extensions
@@ -38,15 +44,23 @@ $container['view'] = function (\Slim\Container  $c) {
     $view->addExtension(new Twig_Extension_Debug());
     $view->addExtension(new Twig_Extension_Profiler($c['twig_profile']));
 
-    //$view->getEnvironment()->addGlobal("current_path", $c["request"]->getUri()->getPath());
     $view->getEnvironment()->addGlobal("app_name", getenv('APP_NAME'));
-
+    // @todo[vanch3d] check https://github.com/kanellov/slim-twig-flash
+    $view->getEnvironment()->addGlobal('flash', $c->flash);
+    $auth = [
+        'check' => $c->auth->check(),
+        'user' => $c->auth->user(),
+        'isAdmin' => $c->auth->isAdmin(),
+        'roles' => $c->auth->roles()
+    ];
+    $view->getEnvironment()->addGlobal('auth', $auth);
+    \Tracy\Debugger::barDump($auth,"auth");
     return $view;
 };
 
 // monolog
-$container['logger'] = function (\Slim\Container $c) {
-    $settings = $c->get('settings')['logger'];
+$container['logger'] = function (Container $c) {
+    $settings = $c->settings['logger'];
     $logger = new Monolog\Logger($settings['name']);
     $logger->pushProcessor(new Monolog\Processor\UidProcessor());
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
@@ -85,51 +99,76 @@ $container['db'] = function() use ($capsule) {
     return $capsule;
 };
 
+// Add Sentinel
+$container['hasher'] = function () {
+    return new Cartalyst\Sentinel\Hashing\BcryptHasher;
+};
+
+$container['dispatcher'] = function () {
+    return new Illuminate\Events\Dispatcher;
+};
+
+$container['sentinel'] = function ($container) {
+    $sentinel = (new \Cartalyst\Sentinel\Native\Facades\Sentinel())->getSentinel();
+    $sentinel->setUserRepository(
+        new \Cartalyst\Sentinel\Users\IlluminateUserRepository(
+            $container['hasher'],
+            $container['dispatcher'],
+            User::class
+        )
+    );
+    return $sentinel;
+};
+
+$container['auth'] = function($container) {
+    return new Auth($container);
+};
+
 
 // -----------------------------------------------------------------------------
 // Health Service Providers
 // -----------------------------------------------------------------------------
 
 /**
- * @param \Slim\Container $c
+ * @param Container $c
  * @return DataProvider\DropboxCached
  * @throws Exception
  * @throws \Interop\Container\Exception\ContainerException
  */
-$container['dropbox'] = function (\Slim\Container  $c) {
+$container['dropbox'] = function (Container  $c) {
     $settings = $c->get('settings');
     return new DataProvider\DropboxCached($settings['dropbox'],$c->logger);
 };
 
 
 /**
- * @param \Slim\Container $c
+ * @param Container $c
  * @return DataWrangler\Mood
  * @throws \Interop\Container\Exception\ContainerException
  */
-$container['mood'] = function (\Slim\Container  $c) {
+$container['mood'] = function (Container  $c) {
     $settings = $c->get('settings');
     $dropbox = $c->get('dropbox');
     return new DataWrangler\Mood($settings['dropbox'],$c->logger,$dropbox);
 };
 
 /**
- * @param \Slim\Container $c
+ * @param Container $c
  * @return DataWrangler\Sleep
  * @throws \Interop\Container\Exception\ContainerException
  */
-$container['sleep'] = function (\Slim\Container  $c) {
+$container['sleep'] = function (Container  $c) {
     $settings = $c->get('settings');
     $dropbox = $c->get('dropbox');
     return new DataWrangler\Sleep($settings['dropbox'],$c->logger,$dropbox);
 };
 
 /**
- * @param \Slim\Container $c
+ * @param Container $c
  * @return DataWrangler\Diet
  * @throws \Interop\Container\Exception\ContainerException
  */
-$container['diet'] = function (\Slim\Container  $c) {
+$container['diet'] = function (Container  $c) {
     $settings = $c->get('settings');
     $dropbox = $c->get('dropbox');
     $diet =new DataWrangler\Diet($settings['dropbox'],$c->logger,$dropbox);
@@ -144,12 +183,12 @@ $container['diet'] = function (\Slim\Container  $c) {
 };
 
 /**
- * @param \Slim\Container $c
+ * @param Container $c
  * @return DataWrangler\Health
  * @throws Exception
  * @throws \Interop\Container\Exception\ContainerException
  */
-$container['activity'] = function (\Slim\Container  $c) {
+$container['activity'] = function (Container  $c) {
     $settings = $c->get('settings');
     $dropbox = $c->get('dropbox');
     return new DataWrangler\Health($settings['dropbox'],$c->logger,$dropbox);
@@ -159,7 +198,7 @@ $container['activity'] = function (\Slim\Container  $c) {
 // External Tools
 // -----------------------------------------------------------------------------
 
-$container['fatsecret'] = function (\Slim\Container  $c) {
+$container['fatsecret'] = function (Container  $c) {
     $settings = $c->get('settings');
     return new Tools\FatSecret($settings['fatsecret'],$c->logger);
 };
