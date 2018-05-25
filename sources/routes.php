aaeafef1ab1ed3eb863b3cf47669ab6252e2f31f
vanch3d <nicolas.github@calques3d.org>
2018-05-25 14:03:39 +0100

    added user auth routes refactored route groups and middleware

--- sources/routes.php
+++ sources/routes.php
@@ -6,24 +6,38 @@
  * Time: 23:17
  */
 
+use Cartalyst\Sentinel\Users\IlluminateUserRepository;
 use NVL\Controllers\APIController;
+use NVL\Controllers\AuthController;
 use NVL\Controllers\VisController;
 use Slim\Http\Request;
 use Slim\Http\Response;
 use Tracy\Debugger;
 
+// -----------------------------------------------------------------------------
+// Local middleware
+// -----------------------------------------------------------------------------
 
-/**
- * Local Middlewares
- */
+$configMiddleware = function (Request $request, Response $response, callable $next) {
+    // add application preference to globals
+    $cfg = \NVL\App::getAppPreferences();
+    $this->view->getEnvironment()->addGlobal("config", $cfg);
+    return $next($request, $response);
+};
 
+// -----------------------------------------------------------------------------
+// Web App routes
+// -----------------------------------------------------------------------------
 
-/**
- * Web App Routes
- */
-$app->group('',function() {
+// Open routes
+$app->group('',function()
+{
+    $this->get('/user/register', AuthController::class . ':getRegister')->setName('user.register');
+    $this->post('/user/register', AuthController::class . ':postRegister')->setName('user.register.post');
+    $this->get('/user/login', AuthController::class . ':getLogin')->setName('user.login');
+    $this->post('/user/login', AuthController::class . ':postLogin');
+    $this->get('/user/logout', AuthController::class . ':getLogout')->setName('user.logout');
 
-    // Main routes
     $this->get('/', function ($request, $response, $args) {
         return $this->view->render($response, 'pages/home.twig');
     })->setName('home');
@@ -31,6 +45,12 @@ $app->group('',function() {
     $this->get('/swagger', function ($request, $response, $args) {
         return $this->view->render($response, 'pages/swagger.ui.twig');
     })->setName('swagger.ui');
+
+});
+
+// User-restricted routes
+$app->group('',function() {
+    // Main routes
 
     $this->group('/diet',function() {
 
@@ -49,11 +69,6 @@ $app->group('',function() {
             ]);
         })->setName('vis.diet.sequence');
 
-    })->add(function (Request $request, Response $response, callable $next) {
-        // add application preference to globals
-        $cfg = \NVL\App::getAppPreferences();
-        $this->view->getEnvironment()->addGlobal("config", $cfg);
-        return $next($request, $response);
     });
 
     $this->get('/calendar', VisController::class . ':showCalendar')->setName('vis.calendar');
@@ -64,8 +79,10 @@ $app->group('',function() {
         return $this->view->render($response, 'visualisation/activity.overview.twig');
     })->setName('vis.activity');
 
-});
+})->add($configMiddleware)
+  ->add(new \NVL\Middleware\UserMiddleware($container));
 
+// Admin-restricted routes
 $app->group('/admin',function() {
 
     $this->get('', function ($request, $response, $args) {
@@ -96,12 +113,8 @@ $app->group('/admin',function() {
         ]);
     })->setName('admin.test');
 
-})->add(function (Request $request, Response $response, callable $next) {
-    // add application preference to globals
-    $cfg = \NVL\App::getAppPreferences();
-    $this->view->getEnvironment()->addGlobal("config", $cfg);
-    return $next($request, $response);
-});
+})->add($configMiddleware)
+  ->add(new \NVL\Middleware\AdminMiddleware($container));
 
 
 $app->group('/api',function() {
@@ -127,7 +140,9 @@ $app->group('/api',function() {
  */
 
 $app->add(new \RunTracy\Middlewares\TracyMiddleware($app));
+
 //$app->add(new \Slim\HttpCache\Cache('public', 86400));
+
 $app->add(function (Request $request, Response $response, callable $next) {
     $route = $request->getAttribute('route');
     if (!empty($route)) {
@@ -136,6 +151,32 @@ $app->add(function (Request $request, Response $response, callable $next) {
     }
     return $next($request, $response);
 });
+
+$app->add(function (Request $request, Response $response, callable $next) {
+    /** @var \NVL\Container $this */
+    /** @var IlluminateUserRepository $rep */
+    /** @var \NVL\Models\User $instance */
+    /** @var \Illuminate\Database\Eloquent\Builder $query */
+
+    $count = $this->sentinel->getUserRepository()
+        ->createModel()
+        ->newQuery()
+        ->count();
+
+    $route = $request->getAttribute('route');
+    if ($route !== null)
+    {
+        $routeName = $route->getName();
+        // if no admin registered, redirect to registration
+        if ($count===0 && ($routeName !== "user.register" && $routeName !== "user.register.post"))
+        {
+            $this->session->set("register.admin",true);
+            return $response->withRedirect($this->router->pathFor('user.register'));
+        }
+    }
+    return $next($request, $response);
+});
+
 
 
 
